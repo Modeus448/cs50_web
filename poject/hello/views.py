d28959684f7322d3a51aafca89385df3edfa933f
Bohdan Hrebinichenko <gbsgdan448@gmail.com>
2026-04-29 09:16:14 +0200

    Робоча версія hello

--- poject/hello/views.py
+++ poject/hello/views.py
@@ -1,7 +1,9 @@
 #Обираємо що баче користувач, який зайшов на сайт
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request): #request -це запит користувача, який зайшов на сайт
-    return render(request, 'index.html') #render - це функція, яка повертає HTML сторінку,
-            #яку ми хочемо показати користувачу. 'index.html' - це назва файлу, який ми хочемо показати користувачу.
+         #return render(request), 'index.html') #render - це функція, яка приймає запит користувача і назву шаблону,
+    return HttpResponse("Hello, world!")            
+        #яку ми хочемо показати користувачу. 'index.html' - це назва файлу, який ми хочемо показати користувачу.
 
--- poject/poject/urls.py
+++ poject/poject/urls.py
@@ -1,8 +1,9 @@
 #Таблиця URL-адрес для проекту Django. 
 #Вона визначає, які URL-адреси будуть оброблятися якими функціями або класами у вашому додатку. 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', include("hello.urls")), #Посилання на додаток hello
 ]
