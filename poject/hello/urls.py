9fd8bb2e96feb3aa2a6750ec36594e1b15f818ea
Bohdan Hrebinichenko <gbsgdan448@gmail.com>
2026-04-29 09:43:09 +0200

    Додані нові сторінки і templates

--- poject/hello/urls.py
+++ poject/hello/urls.py
@@ -1,6 +1,12 @@
+#Тута ми прописуємо їхні адреси для того щоб користувачі могли переходити на різні сторінки нашого сайту.
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),#Посилання на головну сторінку
+    path("names", views.names, name="names"),
+    #("назва в посилані", views.назва функції, name="назва в коді")
+    path("<str:name>", views.greet, name="greet")
+    #("<str:name>" - це змінна, яка буде передаватися в функцію greet. 
+    # Вона може бути будь-яким рядком, який ми передаємо в URL-адресі.
 ]
--- poject/hello/views.py
+++ poject/hello/views.py
@@ -1,9 +1,19 @@
 #Обираємо що баче користувач, який зайшов на сайт
+#Тута ми прописуємо файли які будуть показані і їхні назви в коді
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request): #request -це запит користувача, який зайшов на сайт
-         #return render(request), 'index.html') #render - це функція, яка приймає запит користувача і назву шаблону,
-    return HttpResponse("Hello, world!")            
+    return render(request, 'hello/index.html') #render - це функція, яка редерить на запити щось,
+        #return HttpResponse("Hello, world!")            
         #яку ми хочемо показати користувачу. 'index.html' - це назва файлу, який ми хочемо показати користувачу.
 
+def names(request):
+#Функція назва функції (запит)
+    return HttpResponse("My name is Bohdan")
+    #вивести на екран текст "My name is Bohdan"
+
+
+def greet(request, name):
+    return HttpResponse(f"Hello, {name.capitalize()}!")
+    #вивести на екран текст "Hello, {name}!", де {name} - це ім'я, яке ми передаємо в URL-адресі.
