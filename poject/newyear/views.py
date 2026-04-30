5a1ff2fa247c8a8fccd1a4df1495087116c002b8
Bohdan Hrebinichenko <gbsgdan448@gmail.com>
2026-04-30 09:57:50 +0200

    Доданий add але є баг

--- poject/newyear/views.py
+++ poject/newyear/views.py
@@ -1,3 +1,8 @@
+import datetime
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    now = datetime.datetime.now() #Створуємо зміну для дати
+    return render(request, "newyear/index.html", {
+        "newyear": now.month == 1 and now.day == 1 }), 
+            #получаємо місять і порівнуємо і получаємо день і порівнуєм її 
--- poject/poject/urls.py
+++ poject/poject/urls.py
@@ -1,9 +1,11 @@
 #Таблиця URL-адрес для проекту Django. 
 #Вона визначає, які URL-адреси будуть оброблятися якими функціями або класами у вашому додатку. 
+#Также тута портібно прописувати add urls для корккної роботи
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', include("hello.urls"))
+    path('hello/', include("hello.urls")),
+    path("newyear/", include("newyear.urls")),
 ]
