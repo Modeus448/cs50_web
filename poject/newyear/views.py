02224eda4d2f47fbfee47018ddd7c384717550cf
Bohdan Hrebinichenko <gbsgdan448@gmail.com>
2026-05-12 13:51:48 +0200

    Баг виправлено

--- poject/newyear/views.py
+++ poject/newyear/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 def index(request):
     now = datetime.datetime.now() #Створуємо зміну для дати
     return render(request, "newyear/index.html", {
-        "newyear": now.month == 1 and now.day == 1 }), 
+        "newyear": now.month == 1 and now.day == 1 })
             #получаємо місять і порівнуємо і получаємо день і порівнуєм її 
