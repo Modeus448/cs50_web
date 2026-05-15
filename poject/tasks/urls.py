cfefdf18bc308ae376170f1d44412302f5e1d011
Bohdan Hrebinichenko <gbsgdan448@gmail.com>
2026-05-15 22:01:31 +0200

    Убранк повторення і додвні кноаки лоя переходу між додатками

--- poject/tasks/urls.py
+++ poject/tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'tasks' #дрдав імя додатка для уникнення конфлікту
 urlpatterns = [
     path('', views.index, name='index'),
     path('add', views.add, name='add')
