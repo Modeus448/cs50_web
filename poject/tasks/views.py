82524bc2e660a163bf3f2d7db3bb05572b593b03
Bohdan Hrebinichenko <gbsgdan448@gmail.com>
2026-05-19 12:35:54 +0200

    Переписні коментврі і додане повернення пвсля додавання даних

--- poject/tasks/views.py
+++ poject/tasks/views.py
@@ -1,16 +1,30 @@
 from django import forms
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-tasks = ["foo", "bar", "baz"]
+tasks = []
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task") #створюємо форму для додавання нових завдань
-    priority = forms.IntegerField(label="Priority", min_value=1, max_value=4) #додаємо поле для пріоритету завдання
+    # priority = forms.IntegerField(label="Priority", min_value=1, max_value=4) #додаємо поле для пріоритету завдання
 
 def index(request):
     return render(request, 'tasks/index.html', 
         {"tasks": tasks})
+
 def add(request):
+    #Перевірка на сервері
+    if request.method == "POST":
+        form = NewTasksForm(request.POST) #створюємо екземпляр форми з даними, отриманими від користувача
+        if form.is_valid(): #перевіряємо, чи дані є валідними
+            task = form.cleaned_data #отримуємо дані з форми
+            tasks.append(task) #додаємо нове завдання до списку завдань
+            return HttpResponseRedirect(reverse("tasks:index")) #перенаправляємо користувача на головну сторінку
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form}) #якщо дані не є валідними, повертаємо форму з помилками для виправлення користувачем
+#просто рендеремо сторінку
     return render(request, 'tasks/add.html', {
         "form": NewTaskForm()})
 
