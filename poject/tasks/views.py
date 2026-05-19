42ee47da85e488b93313b050a10c6a1bdfbc71ee
Bohdan Hrebinichenko <gbsgdan448@gmail.com>
2026-05-19 12:28:27 +0200

    Додана перевірка даних як на клвєнті так і на сервері

--- poject/tasks/views.py
+++ poject/tasks/views.py
@@ -1,11 +1,17 @@
+from django import forms
 from django.shortcuts import render
 
 tasks = ["foo", "bar", "baz"]
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task") #створюємо форму для додавання нових завдань
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=4) #додаємо поле для пріоритету завдання
 
 def index(request):
     return render(request, 'tasks/index.html', 
         {"tasks": tasks})
 def add(request):
-    return render(request, 'tasks/add.html')
+    return render(request, 'tasks/add.html', {
+        "form": NewTaskForm()})
 
 # Create your views here.
